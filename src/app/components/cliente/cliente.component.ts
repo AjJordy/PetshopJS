dd165a9a3b96f80317d49d3d959b7a16a15e891d
Jordy Aparecido Faria de Araújo <jordyfaria@hotmail.com>
2019-01-05 11:39:20 -0200

    Começando backend

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -11,4 +11,20 @@ export class ClienteComponent implements OnInit {
   ngOnInit() {
   }
 
+  cadastrar(){
+    // Sending and receiving data in JSON format using POST method
+    var xhr = new XMLHttpRequest();
+    var url = "/register";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json.email + ", " + json.password);
+        }
+    };
+    var data = JSON.stringify({});
+    xhr.send(data);
+  }
+
 }
